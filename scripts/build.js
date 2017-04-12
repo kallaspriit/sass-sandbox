50ef16966a335086a14ecdb80842b3542e08ac22
Priit Kallas <kallaspriit@gmail.com>
2017-04-12 16:37:58 +0300

    Improved production build.

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,7 @@
 import webpack,  { DefinePlugin } from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
 import CleanWebpackPlugin from 'clean-webpack-plugin';
+import ExtractTextWebpackPlugin from 'extract-text-webpack-plugin';
 import colors from 'colors';
 import webpackConfig from '../webpack.config';
 
@@ -12,24 +13,54 @@ webpackConfig.plugins.unshift(
 		webpackConfig.output.path,
 	], {
 		root: webpackConfig.context,
-		verbose: true
+		verbose: false,
 	}),
 
 	// add the environment define plugin
 	new DefinePlugin({
 		'process.env.NODE_ENV': JSON.stringify('production')
-    })
+    }),
+
+	// add plugin to extract
+	new ExtractTextWebpackPlugin({
+		filename: 'bundle.css',
+	}),
 );
 
-const compiler = webpack(webpackConfig);
+// extract css
+// const sassRule = webpackConfig.module.rules.find(rule => rule.test.toString() === '/\\.scss$/');
+// sassRule.use = ExtractTextWebpackPlugin.extract(sassRule.use);
 
+// overwrite the use parameter for css rule to extract css
+webpackConfig.module.rules = webpackConfig.module.rules.map(rule => {
+	// only modify the sass (scss) rule
+	if (rule.test.toString().indexOf('.scss') === -1) {
+		return rule;
+	}
+
+	// add the extracter
+	rule.use = ExtractTextWebpackPlugin.extract({
+		fallback: 'style-loader',
+		use: rule.use.filter(rule => rule.loader !== 'style-loader').map(rule => rule.loader)
+	});
+
+	return rule;
+});
+
+// create compiler
+const compiler = webpack(webpackConfig);
+const startTime = Date.now();
+
+// run the compiler generating production build
 compiler.run((error, stats) => {
 	if (error) {
-		console.log('failed'.red);
+		console.log('FAILED'.red);
 		console.error(error);
 
 		return;
 	}
 
-	console.log('success'.green);
+	const timeTaken = Date.now() - startTime;
+
+	console.log('SUCCESS'.green + ` in ${timeTaken}ms`);
 })
--- webpack.config.js
+++ webpack.config.js
@@ -76,11 +76,11 @@ const config = {
                 test: /\.scss$/,
                 include: includePaths,
                 use: [{
-                    loader: 'style-loader' // creates style nodes from JS strings
+                    loader: 'style-loader'
                 }, {
-                    loader: 'css-loader' // translates CSS into CommonJS
+                    loader: 'css-loader'
                 }, {
-                    loader: 'sass-loader' // compiles Sass to CSS
+                    loader: 'sass-loader'
                 }]
             }
         ]
