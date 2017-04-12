224110e19e7b0ba74d45e83deec57f5c7b17dd76
Priit Kallas <kallaspriit@gmail.com>
2017-04-12 16:08:48 +0300

    Improved production build.

--- scripts/build.js
+++ scripts/build.js
@@ -1,23 +1,35 @@
 import webpack,  { DefinePlugin } from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
-import config from '../webpack.config';
+import CleanWebpackPlugin from 'clean-webpack-plugin';
+import colors from 'colors';
+import webpackConfig from '../webpack.config';
 
 // add production-specific plugins
-config.plugins.unshift(
+webpackConfig.plugins.unshift(
+
+	// add plugin to clean the output folder
+	new CleanWebpackPlugin([
+		webpackConfig.output.path,
+	], {
+		root: webpackConfig.context,
+		verbose: true
+	}),
+
 	// add the environment define plugin
 	new DefinePlugin({
 		'process.env.NODE_ENV': JSON.stringify('production')
     })
 );
 
-const compiler = webpack(config);
+const compiler = webpack(webpackConfig);
 
 compiler.run((error, stats) => {
 	if (error) {
+		console.log('failed'.red);
 		console.error(error);
 
 		return;
 	}
 
-	console.log('success');
+	console.log('success'.green);
 })
